bdd731321470226d8ad9d1583ffded92ad155f5e
KaranRansing2002 <ransingkaran495@gmail.com>
2022-12-05 13:00:47 +0530

    second commit

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,75 +7,83 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import charmander from './images/charmander.png'
 
 const columns = [
-    { id: 0, 
-        label: '', 
-        minWidth: 170 
+    { 
+        id: 0, 
+        label: 'image', 
+        minWidth: 50 
     },
     { 
         id: 1,
         label: 'name', 
-        minWidth: 100 
+        minWidth: 50 
     },
     {
       id: 2,
       label: 'Types',
-      minWidth: 170,
-      align: 'right',
+      minWidth: 100,
+      align: 'center',
     },
     {
       id: 3,
       label: 'abilities',
-      minWidth: 170,
-      align: 'right',
+      minWidth: 100,
+      align: 'center',
     },
     {
         id : 4,
         label : 'HP',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
     },
     {
         id : 5,
         label : 'Atk',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
     },
     {
         id : 6,
         label : 'Def',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
     },
     {
         id : 7,
         label : 'Spa',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
     },
     {
         id : 8,
         label : 'Spd',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
     },
     {
         id : 9,
         label : 'Spe',
-        minWidth : 50,
-        align : 'right'
+        minWidth : 20,
+        align: 'center'
+    },
+    {
+        id : 10,
+        label : 'Bst',
+        minWidth : 20,
+        align: 'center'
     }
 ];
 
 const rows = [
-    ["https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png","Charmander","Fire","Blaze,Solarpower",""]
+    [charmander,"Charmander","Fire","Blaze | Solarpower","39","52","43","60","50","65","309"]
 ]
 
 function Pokedex() {
   return (
-    <div className='h-full'>
-        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+    <div className='h-1/2'>
+        <Paper sx={{ width: '70%', overflow: 'hidden' }}>
         <TableContainer sx={{ maxHeight: 440 }}>
             <Table stickyHeader aria-label="sticky table">
             <TableHead>
@@ -93,25 +101,26 @@ function Pokedex() {
             </TableHead>
             <TableBody>
                 {
-                    rows.map((row)=>{
-                        return 
-                        <TableRow>
-
-                        </TableRow>
+                    rows.map((row,index)=>{
+                        return(
+                            <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                                {
+                                    columns.map((ele,index)=>{
+                                        return (
+                                            <TableCell key={index} align={ele.align}>
+                                                {ele.id==0 ? <img className='h-14 cover' src={row[0]}></img> : row[ele.id]}
+                                            </TableCell>
+                                        )
+                                    })
+                                }
+                            </TableRow>
+                        )
                     })
                 }
             </TableBody>
             </Table>
         </TableContainer>
-        <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+        
         </Paper>
     </div>
   )
