9d12096d55111c46f33847fd97ddffbeea800e25
KaranRansing2002 <ransingkaran495@gmail.com>
2023-12-04 11:38:41 +0530

    cors changes

--- pokemon-backend/App.js
+++ pokemon-backend/App.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", urls[1]);
+  res.header("Access-Control-Allow-Origin", urls[2]);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
