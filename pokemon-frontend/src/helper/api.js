ffb571f9b733cb37de8e14a869caa1632475f8f5
Karan Ransing <ransingkaran495@gmail.com>
2024-08-30 00:44:53 +0530

    adding new api endpoints

--- pokemon-frontend/src/helper/api.js
+++ pokemon-frontend/src/helper/api.js
@@ -1,5 +1,5 @@
-const urls = ['http://localhost:8000', 'https://pokemonbs.onrender.com', 'https://pokeapi-4khl.onrender.com']
+const urls = ['http://localhost:8000', 'https://pokemonbs.onrender.com', 'https://pokeapi-4khl.onrender.com',"https://pokes-api.vercel.app"]
 
-const api = urls[2];
+const api = urls[3];
 
 export default api 
