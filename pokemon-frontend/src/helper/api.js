e89c9622a7870135de40fe8c967d2192b9be4343
Karan Ransing <75631561+KaranRansing2002@users.noreply.github.com>
2025-11-13 09:43:57 +0530

    Change API URL to use the third endpoint

--- pokemon-frontend/src/helper/api.js
+++ pokemon-frontend/src/helper/api.js
@@ -1,5 +1,5 @@
 const urls = ['http://localhost:8000', 'https://pokemonbs.onrender.com', 'https://pokeapi-4khl.onrender.com',"https://pokes-api.vercel.app"]
 
-const api = urls[1];
+const api = urls[2];
 
 export default api 
